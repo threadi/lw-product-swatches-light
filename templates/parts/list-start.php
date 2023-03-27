0535959a1ae68115e9f7e5595c300bba854a3b0a
threadi <tzwirne@gmx.de>
2023-03-27 16:18:00 +0200

    add swatches changes on gallery change

--- templates/parts/list-start.php
+++ templates/parts/list-start.php
@@ -4,4 +4,4 @@
  * Output of starting swatch-list.
  */
 
-echo '<ul class="lw_product_swatches lw_product_swatches_'.esc_attr($typenames).'" data-type="'.esc_attr($typename).'" data-attribute="'.esc_attr($taxonomy).'">';
+echo '<ul class="lw_product_swatches lw_product_swatches_'.esc_attr($typenames).'" data-type="'.esc_attr($typename).'" data-attribute="'.esc_attr($taxonomy).'" data-changed-by-gallery="'.esc_attr($changed_by_gallery).'">';
