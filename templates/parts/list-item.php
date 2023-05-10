352616b117ffa1af1812cf909d1b903caa9cb8ec
threadi <tzwirne@gmx.de>
2023-05-10 10:34:27 +0200

    replace sanitize in output

--- templates/parts/list-item.php
+++ templates/parts/list-item.php
@@ -4,4 +4,4 @@
  * Output of single linked swatch item in list.
  */
 
-echo '<li><span class="'.esc_attr($class). '" title="' . sanitize_text_field($title) . '" style="'.esc_attr($css).'" data-image="' . esc_attr($image) . '" data-image-srcset="' . esc_attr($srcset) . '" data-sale="' . esc_attr($sale) . '"></span></li>';
+echo '<li><span class="'.esc_attr($class). '" title="' . esc_attr($title) . '" style="'.esc_attr($css).'" data-image="' . esc_attr($image) . '" data-image-srcset="' . esc_attr($srcset) . '" data-sale="' . esc_attr($sale) . '"></span></li>';
